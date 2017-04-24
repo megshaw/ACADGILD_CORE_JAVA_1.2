763c8afa70237440d51165326d194be11b6bd8b0
meghana <meghana@localhost>
2017-04-24 17:19:01 +0530

    Java Assignment 1.2has been updated

---
+++ Java Assignment 1.2/src/JavaAssignment12.java
@@ -0,0 +1,25 @@
+
+
+
+public class JavaAssignment12 {
+
+    public static void main(String[] args) {
+   
+ 
+       int a=20;
+       int b=10;
+        b= a-- - --a;
+        System.out.println("b=" +b);
+        
+        int c=a--;
+         System.out.println("c=" +c);
+         int d=a>>2;
+         System.out.println("d=" +d);
+         
+         int e=a&b;
+          System.out.println("e=" +e);
+         
+        
+    }
+    
+}
